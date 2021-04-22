a3f3dea7542c771015de2fc6c8f4ca9048eb5813
Viktor Kushnerov <filimo@gmail.com>
2021-04-22 20:37:32 +0300

    Initial Commit

---
+++ Shared/CryptoCombineApp.swift
@@ -0,0 +1,20 @@
+//
+//  CryptoCombineApp.swift
+//  Shared
+//
+//  Created by Viktor Kushnerov on 22.04.21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCombineApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
